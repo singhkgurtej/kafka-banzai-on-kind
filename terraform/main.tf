e42291479f592aea371eb5246968ebef8f1076dd
gurtej <gskotra@gmail.com>
2024-12-29 21:38:58 -0500

    changed repo to private

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,7 +50,7 @@ resource "kind_cluster" "default" {
 resource "github_repository" "terraform-flux" {
   name        = var.github_repository
   description = var.github_repository
-  visibility  = "public"
+  visibility  = "private"
   auto_init   = true # This is extremely important as flux_bootstrap_git will not work without a repository that has been initialised
 }
 
